c2a426eb22565d8da499f896566b86dd9b16138c
Ella Smith <ellasmith@mac.home>
2025-06-10 17:08:30 +0100

    Update CORS to allow Netlify

--- server.js
+++ server.js
@@ -28,14 +28,7 @@ const allowedOrigins = ["http://localhost:1234", "https://chinwag.netlify.app"];
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin like mobile apps or curl
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: "*",
     credentials: true,
   })
 );
